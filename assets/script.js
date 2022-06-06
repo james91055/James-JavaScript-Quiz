0349244868ca085ad4349bbce04cb207a0748149
James <james@macbook-air.lan>
2022-06-06 04:10:22 -0700

    update quiz

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,14 @@ var startBut = document.getElementById("start-button");
 var infoEl = document.getElementById("start-info");
 var endEl = document.getElementById("end-info");
 var highscoreEl = document.getElementById("highscore");
-const quizData = [
+var initialInput = document.getElementById("initials");
+
+var questionEl = document.getElementById("question");
+var a_text = document.getElementById("a_text");
+var b_text = document.getElementById("b_text");
+var c_text = document.getElementById("c_text");
+var d_text = document.getElementById("d_text");
+var quizData = [
   {
     question: "1+1  = ",
     a: "0",
@@ -46,14 +53,33 @@ const quizData = [
     correct: "b",
   },
 ];
+
+let currentQuiz = 0;
+
+function loadQuiz() {
+  var currentQuizData = quizData[currentQuiz];
+  if (currentQuiz < quizData.length) {
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+    currentQuiz++;
+  } else {
+    submitInitials();
+    clearInterval(timeInterval);
+  }
+}
+
 function displayMessage() {}
 
 function countdown() {
   var timeLeft = 30;
-  quizEl.style.display = "block";
   infoEl.style.display = "none";
-  highscoreEl.style.display = "none";
+  quizEl.style.display = "block";
   endEl.style.display = "none";
+  initialInput.style.display = "none";
+  highscoreEl.style.display = "none";
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
     timeLeft--;
@@ -62,21 +88,32 @@ function countdown() {
       clearInterval(timeInterval);
 
       timerEl = "";
-      endEl.style.display = "block";
+      infoEl.style.display = "none";
       quizEl.style.display = "none";
+      endEl.style.display = "block";
+      initialInput.style.display = "none";
+      highscoreEl.style.display = "none";
     }
   }, 1000);
 }
 
 function startgame() {
-  quizEl.style.display = "none";
   infoEl.style.display = "block";
+  quizEl.style.display = "none";
   endEl.style.display = "none";
+  initialInput.style.display = "none";
   highscoreEl.style.display = "none";
+
+  loadQuiz();
 }
-function submitInitiasl() {
+function submitInitials() {
+  infoEl.style.display = "none";
+  quizEl.style.display = "none";
   endEl.style.display = "none";
+  initialInput.style.display = "none";
   highscoreEl.style.display = "block";
+  let currentQuiz = 0;
+  console.log(initialInput);
 }
 
 startgame();

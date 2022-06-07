a135c013e3fa77604634bf989603ebc04b8dcf43
James <james@MacBook-Air.attlocal.net>
2022-06-06 17:02:58 -0700

    update js highscore

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # James-JavaScript-Quiz
+
+## Description
+
+### What This Does
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+### Technology Used
+
+- HTML
+
+  - Onclick event
+  - Arrange different blocks and interact with Javascript
+
+- CSS
+  - Optimize UI interface for
+- JavaScript
+
+## Features in Password Genarator
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## Links and Credits
+
+- [ASCII Table](https://www.asciitable.com/)
+- [Merge Arrays in JavaScript](https://www.samanthaming.com/tidbits/49-2-ways-to-merge-arrays/)
+- [Using Math.random() in JavaScript](https://josephcardillo.medium.com/using-math-random-in-javascript-c49eff920b11)
--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,24 @@ var startBut = document.getElementById("start-button");
 var infoEl = document.getElementById("start-info");
 var endEl = document.getElementById("end-info");
 var highscoreEl = document.getElementById("highscore");
-var initialInput = document.getElementById("initials");
+
+var highscoreData = document.getElementById("highscore-data");
 
 var questionEl = document.getElementById("question");
 var a_text = document.getElementById("a_text");
 var b_text = document.getElementById("b_text");
 var c_text = document.getElementById("c_text");
 var d_text = document.getElementById("d_text");
+
+var timeLeft = 30;
+
 var quizData = [
   {
-    question: "1+1  = ",
-    a: "0",
-    b: "1",
-    c: "2",
-    d: "3",
+    question: "Inside which HTML element do we put the JavaScript? ",
+    a: " <javascript>",
+    b: "<js>",
+    c: "<script>",
+    d: "<scripting>",
     correct: "c",
   },
   {
@@ -67,31 +71,31 @@ function loadQuiz() {
     currentQuiz++;
   } else {
     submitInitials();
-    clearInterval(timeInterval);
+    timerEl = "";
   }
 }
 
 function displayMessage() {}
 
 function countdown() {
-  var timeLeft = 30;
+  loadQuiz();
+
   infoEl.style.display = "none";
   quizEl.style.display = "block";
   endEl.style.display = "none";
-  initialInput.style.display = "none";
   highscoreEl.style.display = "none";
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
+    timeLeft = 30;
     timeLeft--;
     timerEl.textContent = "Time Left: " + timeLeft;
-    if (timeLeft === 0) {
+    if (timeLeft === 0 || currentQuiz === 0) {
       clearInterval(timeInterval);
-
+      timeLeft = 0;
       timerEl = "";
       infoEl.style.display = "none";
       quizEl.style.display = "none";
       endEl.style.display = "block";
-      initialInput.style.display = "none";
       highscoreEl.style.display = "none";
     }
   }, 1000);
@@ -101,19 +105,39 @@ function startgame() {
   infoEl.style.display = "block";
   quizEl.style.display = "none";
   endEl.style.display = "none";
-  initialInput.style.display = "none";
   highscoreEl.style.display = "none";
-
-  loadQuiz();
 }
 function submitInitials() {
   infoEl.style.display = "none";
   quizEl.style.display = "none";
+  endEl.style.display = "block";
+  highscoreEl.style.display = "none";
+}
+
+function highscore() {
+  infoEl.style.display = "none";
+  quizEl.style.display = "none";
   endEl.style.display = "none";
-  initialInput.style.display = "none";
   highscoreEl.style.display = "block";
-  let currentQuiz = 0;
-  console.log(initialInput);
+  var initialInput = document.getElementById("initials");
+  // localStorage.setItem("initials", initialInput);
+  // var highscoreInitial = localStorage.getItem("initials");
+  // console.log(initialInput);
+  // highscoreData.textContent = initialInput + " " + timeLeft;
+
+  var highscoreBoard = {
+    initial: initialInput.value,
+    score: timeLeft,
+  };
+  localStorage.setItem("highscoreBoard", JSON.stingify(highscoreBoard));
+  renderMessage();
+}
+function renderMessage() {
+  var lastscore = JSON.parse(localStorage.getItem("highscoreBoard"));
+  if (lastscore !== null) {
+    document.querySelector("#highscore-data").textContent =
+      lastscore.initial + " " + lastscore.score;
+  }
 }
 
 startgame();
